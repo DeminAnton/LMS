2f23a7f744cc2d20f5fab9c09efcb0a5d69d12af
DeminAnton <demantser@gmail.com>
2024-09-29 14:58:40 +0800

    add prefix param to include_router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 from routers import router as api_router
+from config import settings
 
 app = FastAPI()
-app.include_router(api_router)
+app.include_router(api_router, prefix=settings.prefix.api)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
