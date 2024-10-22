5996a787c6e6113a364167da3964b5b868b054d6
DeminAnton <demantser@gmail.com>
2024-10-22 19:37:33 +0800

    add readme

--- app/models/base.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.orm import declared_attr
-
-
-class Base(DeclarativeBase):
-    __abstract__ = True
--- app/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from routers.user import router as user_router
-from routers.login import router as login_router
-
-router = APIRouter()
-router.include_router(user_router)
-router.include_router(login_router)
-
